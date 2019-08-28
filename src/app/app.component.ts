9fb331d396aabf45e1a31cb6388d20970c2d444d
Carlos Ruben Barrios <cbarrios.ext@Alianza.com.co>
2019-08-28 17:58:17 -0500

    Form navigation. Tab and enter keys

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChildren, AfterViewInit, QueryList, Renderer} from '@angular/core';
+import {MatFormFieldControl} from '@angular/material/form-field';
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit {
+  myControl = new FormControl();
+  options: string[] = ['Delhi', 'Mumbai', 'Banglore'];
+  filteredOptions: Observable<string[]>;
+  navegacion:any[] = [];
+  @ViewChildren(MatFormFieldControl) inputs: QueryList<any>;
+  constructor(private renderer: Renderer) {}
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value))
+    );
+  }
+
+  ngAfterViewInit() {
+    this.navegacion = this.inputs.toArray();
+  }
+
+  onKeydown(event, index) {
+    if(index + 1 < this.navegacion.length){
+      this.renderer.invokeElementMethod(this.navegacion[index + 1]._elementRef.nativeElement, 'focus');
+    }
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.options.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+/* Angular material */
+import { AngularMaterialModule } from './angular-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TabDirective} from './tab-directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
