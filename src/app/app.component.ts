b958c3535d8371128e4ee0b0eb065988c22752f9
williaammolina <wmolina98@gmail.com>
2019-09-09 17:51:21 -0500

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,36 @@ import { FormComponent } from './form.component';
 export class AppComponent extends FormComponent implements OnInit {
   user: string = "KMEDINA";
   username: string = "KAREN MEDINA"
+  oficina: object = {"codigo":"30", "nombre": "BOGOTÁ - MORATO"};
+  fondo: object = {"codigo":"300", "nombre":"Fondo de Pensiones Voluntarias Visión"};
+  encargo: object = {
+    "numero":"600010035",
+    "codigoVerificacion":"9",
+    "persona": "EDGAR FABIAN TORRES SANCHEZ",
+    "identificacion": "79435173",
+    "nombre": "EDGAR FABIAN TORRES SANCHEZ",
+    "codigoPlanInversion": "1",
+    "nombrePlanInversion" : "ABIERTO VISION -INDIVIDUALES"
+  };
+  retiro: object = {
+    "codigo": "RESD",
+    "nombre": "RETIROS SIN DESCUENTOS",
+    "tipoMvto": "385",
+    "nombreMvto": "RETIRO EXENTO (COMISION RECAUDO)"
+  };
+  valor: string = "856000";
+  nroVolante: string = "";
+  fondoExterno: object = {
+    "nit": "123453223",
+    "nombre": "FONDO EXTERNO"
+  };
+  formaPago: object = {
+    "codigo": "CH",
+    "nombre": "Cheque",
+    "operacionesGratis": 2,
+    "condicionPensionado": "N"
+  };
+  observaciones: string = "RETIRO EXENTO (COMISION RECAUDO)";
   fecha: Date = new Date();
   myControl = new FormControl();
   ciudades: any = [];
@@ -22,7 +52,8 @@ export class AppComponent extends FormComponent implements OnInit {
   constructor(protected renderer: Renderer, private apiService: ApiService) {
     super(renderer);
   }
-  ngOnInit() {        
+  ngOnInit() {
+    this.myControl.setValue(this.oficina.codigo);        
     this.apiService.getNews().subscribe((data)=>{
       this.ciudades = data;
       this.filteredOptions = this.myControl.valueChanges.pipe(
@@ -37,10 +68,13 @@ export class AppComponent extends FormComponent implements OnInit {
       //this.renderer.invokeElementMethod(this.navegacion[index + 1]._elementRef.nativeElement, 'focus');
     }
   }
+  select(event, option){
+    if (event.source.selected) this.oficina = option;
+  }
 
   private _filter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.ciudades.filter(option => option.nombre.toLowerCase().indexOf(filterValue) === 0);
+    return this.ciudades.filter(option => option.codigo.toLowerCase().indexOf(filterValue) === 0);
   }
 }
 
